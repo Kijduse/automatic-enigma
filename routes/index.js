e4340697c437515d04a514ab4e4a5e80c4a6600e
Kevin Park <kevinpark@Kevins-MacBook-Pro.local>
2020-02-05 10:26:27 +0000

    basic site

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Kevin J Park Limited' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Kevin J Park Limited :: About' });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Kevin J Park Limited :: Services' });
+});
+
+router.get('/site', function(req, res, next) {
+  res.render('site', { title: 'Kevin J Park Limited :: This Site' });
 });
 
 module.exports = router;
